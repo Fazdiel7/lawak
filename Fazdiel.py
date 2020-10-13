2b765218f8ed46f269d33e9597006631f0f5bbc8
Fazdiel7 <72813931+Fazdiel7@users.noreply.github.com>
2020-10-13 23:10:24 +0700

    Update Fazdiel.py

--- Fazdiel.py
+++ Fazdiel.py
@@ -1,43 +1,180 @@
 #!/usr/bin/bash
-#Ini untuk 
-#pembelajaran
-#Code by Fazdiel_7
+#tools ini
+#prank hack satelit
+#Hasil recode stah
+#Pfft
 clear
-red='\033[34;1m'
-blue='\033[32;1m'
-grey='\033[35;1m'
-orange='\033[36;1m'
-indigo='\033[31;1m'
-white='\033[1;35m'
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+ungu='\033[1;35m'
 echo " "
-echo $blue "_____________________________________"
-echo $red "Code By Fazdiel_7 
-jangan lupa subrek gays 
-echo $blue "_____________________________________"
+echo  $blue "<|∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆|>"
+echo  $cyan "|       TOOLS HACK satelit-V1        |"
+echo  $red    "|                                    |"
+echo  $red    "| Author        : MR.Ngentod        |"
+echo  $red    "| Contact       : pranata_pasha      |"
+echo  $red    "| My Team    : InDoNeSiA Ngentod cyber |"
+echo  $red    "| Thanks To  : MR.ngentod        |"
+echo  $blue "<|∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆|>"
 sleep 1
 echo " "
-echo $green "✓Menu Tools By Fazdiel_7"
-echo $grey "====================================="
-echo $green "1.Buchen :v"
-echo $green "2.quotes sindiran"
-echo $green "3.Untukmu sayang :v"
-echo $grey "====================================="
-echo $indigo "Root@Fazdiel_7" 
-read -p" Lo Mau gw hek "
-if [ $ngehek = 1] || [ $ngehek =01]
+echo $green "============== $red MENU HACK $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. ∆Geser Satelit Palapa ∆      $cyan ||"
+echo $blue "|| $green 2. ∆Geser Satelit Telkom∆       $blue ||"
+echo $blue "|| $green 3. ∆Geser Satelit Asiasat∆     $red  ||"
+echo $blue "|| $green 4. ∆Geser Satelit Nasa∆         $green ||"
+echo $blue "|| $green 0. ∆Exit The Program ∆          $purple ||"
+echo $blue "===================================="
+echo $ungu"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@HACK=∆>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
 then
-echo $blue"bapak lu jagoan mana ngentid"
-read -p" Gak Suka Pc Ster"
+echo $blue"╭──[Mau Kearah Mana cuk]>"
+read -p"╰───────root@HACK=∆>" arah;
 sleep 1
-echo $red "Loanjing"
+echo $green"╭──[Mau Digeser Berapa Derajat CUK]>"
+read -p"╰───────root@HACK=∆>" derajat;
 sleep 1
-echo $pink "10000000000000%"
+echo $red "LOANJING"
 sleep 1
-echo $red "Udah Selesai anjing"
-slep 1
-echo $lime "Kontol bapak kau berhasil di cabut :v"
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasi  Digeser ke arah $arah sejauh $derajat derajat"
 fi
 
-echo $pink "Selamat Belajar asu"
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+echo $ungu"╭──[Mau Kearah Mana CUK]>"
+read -p"╰───────root@HACK=∆>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@HACK=∆>" derajat;
+sleep 1
+echo $red "LOANJING"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done DONE JING''
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Mau Kearah Mana CUK]>"
+read -p"╰───────root@HACK=∆>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat CUK]>"
+read -p"╰───────root@HACK=∆>" derajat;
+sleep 1
+echo $red "LOANJING"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "10000000000000000000000%"
+sleep 1
+echo $green "Prosessing Done JUGA JING"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 4 ] || [ $Gans = 04 ]
+then
+echo $blue"╭──[Mau Kearah Mana CUK]>"
+read -p"╰───────root@HACK=∆>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@HACK=∆>" derajat;
+sleep 1
+echo $red "LOANJING"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done CUK"
+sleep 1
+echo  $blue "Halah memek $lo kontol $Ngentid"
+fi
+
+if [ $ngehek = 0 ] || [ $ngehek = 00 ]
+then
+echo  $red "SELAMAT ANDA TELAH BERHASIL"
+echo  $blue "Jangan Lupa Coli Hari Ini CUK...."
+echo  $green "Warureja Security X ploit"
 exit
 fi
