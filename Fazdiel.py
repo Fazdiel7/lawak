8bdd44027eb60d3afb5542b34cd3899f12378791
Fazdiel7 <72813931+Fazdiel7@users.noreply.github.com>
2020-10-13 22:59:19 +0700

    Update and rename .gitignore to Fazdiel.py

---
+++ Fazdiel.py
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+#Ini untuk 
+#pembelajaran
+#Code by Fazdiel_7
+clear
+red='\033[34;1m'
+blue='\033[32;1m'
+grey='\033[35;1m'
+orange='\033[36;1m'
+indigo='\033[31;1m'
+white='\033[1;35m'
+echo " "
+echo $blue "_____________________________________"
+echo $red "Code By Fazdiel_7 
+jangan lupa subrek gays 
+echo $blue "_____________________________________"
+sleep 1
+echo " "
+echo $green "✓Menu Tools By Fazdiel_7"
+echo $grey "====================================="
+echo $green "1.Buchen :v"
+echo $green "2.quotes sindiran"
+echo $green "3.Untukmu sayang :v"
+echo $grey "====================================="
+echo $indigo "Root@Fazdiel_7" 
+read -p" Lo Mau gw hek "
+if [ $ngehek = 1] || [ $ngehek =01]
+then
+echo $blue"bapak lu jagoan mana ngentid"
+read -p" Gak Suka Pc Ster"
+sleep 1
+echo $red "Loanjing"
+sleep 1
+echo $pink "10000000000000%"
+sleep 1
+echo $red "Udah Selesai anjing"
+slep 1
+echo $lime "Kontol bapak kau berhasil di cabut :v"
+fi
+
+echo $pink "Selamat Belajar asu"
+exit
+fi
